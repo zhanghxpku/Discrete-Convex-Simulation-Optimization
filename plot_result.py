22f5a7dcc36b8fb5d3da4b736a7ae52effba775d
zhanghaixiang <1500010620@pku.edu.cn>
2021-03-26 20:31:00 -0700

    Update plot_result.py

--- plot_result.py
+++ plot_result.py
@@ -16,12 +16,12 @@ from scipy.interpolate import CubicSpline
 #     # "font.sans-serif": ["Helvetica"],
 #     "text.latex.preamble": [r'\usepackage{amsfonts}']})
 
-stat = np.zeros((15,8))
+stat = np.zeros((15,10))
 min_val = np.zeros((15,))
 
 for N in range(10,160,10):
     i = 0
-    results = np.zeros((102,4))
+    results = np.zeros((102,6))
         
     # with open("./results/sqrt_sep/sep_"+str(N)+".txt") as f_in:
     with open("./results/queue/queue_"+str(N)+".txt") as f_in:
@@ -36,6 +36,16 @@ for N in range(10,160,10):
                     results[i,j] = float(line[j])
                 i += 1
     
+    i = 0
+    # with open("./results/sep_ucb/sep_ucb_"+str(N)+".txt") as f_in:
+    with open("./results/queue_ucb/queue_ucb_"+str(N)+".txt") as f_in:
+        for line in f_in:
+            line = line.split(" ")
+            if len(line) == 2:
+                for j in range(2):
+                    results[i,j+4] = float(line[j])
+                i += 1
+    
     # Average simulation runs
     stat[int(N/10)-1,0] = np.mean(results[:-2,0])
     stat[int(N/10)-1,1] = np.std(results[:-2,0])
@@ -48,8 +58,12 @@ for N in range(10,160,10):
     stat[int(N/10)-1,6] = np.mean(results[:-2,3])
     stat[int(N/10)-1,7] = np.std(results[:-2,3])
     
+    # Average simulation runs
+    stat[int(N/10)-1,8] = np.mean(results[:-2,4])
+    stat[int(N/10)-1,9] = np.std(results[:-2,4])
+    
     # Minimal value
-    min_val[int(N/10)-1] = np.min( results[:-2,[2,3]] )
+    # min_val[int(N/10)-1] = np.min( results[:-2,[2,3]] )
 
 # Plot the results
 N_set = np.array([N for N in range(10,160,10)])
@@ -57,6 +71,8 @@ plt.figure()
 curve1, = plt.plot(N_set, stat[:,0], color="navy")
               # yerr=stat[:,1])
 curve2, = plt.plot(N_set, stat[:,2], color="purple")
+              # yerr=stat[:,3])
+curve5, = plt.plot(N_set, stat[:,8], color="darkorange", alpha=0.5)
               # yerr=stat[:,3])
               
 # Plot the variance
@@ -75,6 +91,14 @@ cs1 = CubicSpline(N_set, stat[:,3])
 yfit1 = cs1(xfit)
 plt.fill_between(xfit, yfit-yfit1, yfit+yfit1,color='r', alpha=0.1)
 
+# Plot the variance
+cs = CubicSpline(N_set, stat[:,8])
+xfit = np.linspace(10, 150, 1400)
+yfit = cs(xfit)
+cs1 = CubicSpline(N_set, stat[:,9])
+yfit1 = cs1(xfit)
+plt.fill_between(xfit, yfit-yfit1, yfit+yfit1,color='y', alpha=0.1)
+
 # Plot the trend
 curve3, = plt.plot( N_set, 
                     np.ceil(np.log(0.5*N_set+11))*4.9e4+stat[0,0]-4.6e4*math.log(10),
@@ -85,15 +109,16 @@ curve4, = plt.plot( N_set, 4.6e4 * np.ones(N_set.shape), alpha=0.2, color="r" )
 #                     alpha=0.2, color="b" )
 # curve4, = plt.plot( N_set, 1.1e5 * np.ones(N_set.shape), alpha=0.2, color="r" )
 
-plt.legend([curve1,curve2,curve3,curve4],
-           ["AS","US","y = O([log N])","y = Const"],
-           loc='center right')
+plt.legend([curve1,curve2,curve5,curve3,curve4],
+           ["AS","US","lil'UCB","y = O([log N])","y = Const"] )
 plt.xlabel("N")
 plt.ylabel("Sample complexity")
 # plt.title("Queueing Model")
-plt.savefig("./results/queue/queue.png",bbox_inches='tight', dpi=300)
+plt.ylim( [0, 300000] )
+plt.savefig("./results/queue/queue_new.png",bbox_inches='tight', dpi=300)
 # plt.title("Artificial Convex Model")
-# plt.savefig("./results/sqrt_sep/sep.png",bbox_inches='tight', dpi=300)
+# plt.ylim( [0, 600000] )
+# plt.savefig("./results/sqrt_sep/sep_new.png",bbox_inches='tight', dpi=300)
 
 
 # plt.figure()
