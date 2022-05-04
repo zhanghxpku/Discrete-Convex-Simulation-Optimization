d70b7de3886ddd2a46cb9233cc947005e1e7d497
zhanghxpku <haixiang_zhang@berkeley.edu>
2022-05-04 11:23:44 -0700

    refine coding style

--- models/bus_model.py
+++ models/bus_model.py
@@ -8,14 +8,13 @@ Generate bus scheduling examples.
 
 import numpy as np
 from scipy import stats
-# import matplotlib.pyplot as plt
-import time
 
-def BusModel(params):
+
+def busmodel(params):
     """
     Generate the objective function
     """
-    
+
     # Parameters: lambda
     lam = 10
     # Retrieve parameters: upper bound of time
@@ -23,50 +22,49 @@ def BusModel(params):
     # Retrieve parameters: number of buses
     d = params["d"] if "d" in params else 1
     # The optimal solution
-    x_opt = np.linspace(tau / (d+1), tau * d / (d+1), d) + 1
-        
-    return {"F": lambda y: WaitingTime(y,params,False),
-            "f": lambda y: WaitingTime(y,params,True),
-            "L":lam*tau, "x_opt":x_opt}
+    x_opt = np.linspace(tau / (d + 1), tau * d / (d + 1), d) + 1
+
+    return {"F": lambda y: waitingtime(y, params, False),
+            "f": lambda y: waitingtime(y, params, True),
+            "L": lam * tau, "x_opt": x_opt}
 
 
-def WaitingTime(x,params,expectation=False):
+def waitingtime(x, params, expectation=False):
     """
     Compute the waiting time
     """
-    
+
     # Parameters: lambda
     lam = 10
     # Retrieve parameters: upper bound of time
     tau = params["N"] + 1 if "N" in params else 100
     # Retrieve parameters: number of buses
     d = params["d"] if "d" in params else 1
-    
+
     # Length of intervals
-    length = np.zeros((d+1,))
+    length = np.zeros((d + 1,))
     length[0] = x[0] - 1
     length[1:-1] = np.abs(np.diff(x))
     length[-1] = tau + 1 - x[-1]
-    
+
     if expectation:
-        return lam / 2 * np.sum( length ** 2 )
+        return lam / 2 * np.sum(length ** 2)
     else:
         # Number of arrivals in each interval
         # try:
-        n = stats.poisson.rvs(length,size=(d+1,))
+        n = stats.poisson.rvs(length, size=(d + 1,))
         # except ValueError:
         #     print(length)
         #     return None
-    
+
         # Waiting time
         wait_time = 0
-        
+
         # Compute the waiting time
-        for i in range(d+1):
+        for i in range(d + 1):
             # # Number of arrivals
             # n = stats.poisson.rvs(length[i] * lam)
             # Waiting time
-            wait_time += stats.uniform.rvs(0,n[i] * length[i] / 2)
-        
+            wait_time += stats.uniform.rvs(0, n[i] * length[i] / 2)
+
         return wait_time
-        
