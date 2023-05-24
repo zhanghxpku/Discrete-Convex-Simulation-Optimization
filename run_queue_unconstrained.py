5175aa98818f53d7fdcade8e502435991040e41b
zhanghaixiang_pku <1500010620@pku.edu.cn>
2023-05-23 20:43:32 -0700

    Update run script

--- run_queue_unconstrained.py
+++ run_queue_unconstrained.py
@@ -33,7 +33,7 @@ params["K"] = params["N"] * params["d"]
 # params["trunc"] = bool(int(sys.argv[3]))
 params["trunc"] = True
 # method = int(sys.argv[1])
-method = 2
+method = 3
 
 if method == 0:
     params["eta"] = 5 if params["trunc"] else 0.05
@@ -54,15 +54,15 @@ gaps = np.zeros((2,))
 rate = np.zeros((2,))
 
 # Open the output file
-f_out = open("./results/queue_uncons_" + str(params["N"]) + "_"  + str(params["d"]) + "_" + str(method) + "_" + str(params["eta"]) + ".txt", "w")
+# f_out = open("./results/queue_uncons_" + str(params["N"]) + "_"  + str(params["d"]) + "_" + str(method) + "_" + str(params["eta"]) + ".txt", "w")
 
 for t in range(1):
     print(t)
-    model = models.queueing_or_model.queuereg_model_24hr(params)
+    model = models.queuereg_model_24hr(params)
     
-    f_out.write(str(t))
-    f_out.write("\n")
-    f_out.flush()
+    # f_out.write(str(t))
+    # f_out.write("\n")
+    # f_out.flush()
     
     # Lipschitz constant and closed-form objective function
     if "L" in model:
@@ -101,25 +101,25 @@ for t in range(1):
     
     # Use truncated subgradient descent method
     if method == 0:
-        output_grad = solvers.gradient_solver.GradientProjSolver(model["F"],params,params["trunc"])
+        output_grad = solvers.gradient_proj_solver(model["F"],params,params["trunc"])
     elif method == 1:
-        output_grad = solvers.vaidya_solver.VaidyaProjSolver(model["F"],params)
+        output_grad = solvers.vaidya_proj_solver(model["F"],params)
     elif method == 2:
-        output_grad = solvers.random_walk_solver.RandomWalkProjSolver(model["F"],params)
+        output_grad = solvers.random_walk_proj_solver(model["F"],params)
     elif method == 3:
-        output_grad = solvers.dim_reduction_solver.dimension_reduction_proj_solver(model["F"], params)
+        output_grad = solvers.dimension_reduction_proj_solver(model["F"], params)
     elif method == 4:
         output_grad = solvers.gps_solver.GPS_solver(model["F"], params)
     print(output_grad)
     
     # Update records
     total_samples[0] = ( total_samples[0] * t + output_grad["total"] ) / (t+1)
-
-    f_out.write(str(output_grad["total"]))
-    f_out.write("\n")
-    # f_out.write(str(model["f"](output_grad["x_opt"]) - f_opt))
+    #
+    # f_out.write(str(output_grad["total"]))
     # f_out.write("\n")
-    f_out.flush()
+    # # f_out.write(str(model["f"](output_grad["x_opt"]) - f_opt))
+    # # f_out.write("\n")
+    # f_out.flush()
     
     sample = np.zeros((5000,))
     for i in range(5000):
@@ -127,10 +127,10 @@ for t in range(1):
     print(np.std(sample),np.mean(sample))
     gaps[0] = ( gaps[0] * t + np.mean(sample) ) / (t+1)
 
-f_out.write("\n")
-f_out.write( " ".join([ str(total_samples[0]),str(gaps[0]),str(rate[0]) ]) )
-
-f_out.close()
+# f_out.write("\n")
+# f_out.write( " ".join([ str(total_samples[0]),str(gaps[0]),str(rate[0]) ]) )
+#
+# f_out.close()
 
 
 # sample = np.zeros((5000,))
